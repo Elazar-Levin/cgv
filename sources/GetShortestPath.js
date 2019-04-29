1c6884698a47bcaa195482ee0f708e0ad3b40167
elazaryehuda levin <1504371@host006-07-08.ms.wits.ac.za>
2019-04-29 15:31:46 +0200

    started writing BFS class

---
+++ sources/GetShortestPath.js
@@ -0,0 +1,29 @@
+class GetShortestPath
+{
+	constructor(data)
+	{
+		super();
+		this.data=data;
+		this.visited =[];
+		for(var i=0;i<50;i++)
+		{
+			visited[i]=[];
+		}
+		//this.
+	
+	
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ sources/Pair.js
@@ -0,0 +1,25 @@
+class Pair
+{
+	constructor(x,y)
+	{
+		super();
+		this.x=x;
+		this.y=y;
+	
+	
+	
+	
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+}
